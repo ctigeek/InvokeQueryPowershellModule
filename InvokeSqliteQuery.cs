20788cfb8f381d0b40ee283347c565a1a5174399
Steven Swenson <ctigeek@gmail.com>
2015-01-24 19:54:15 -0500

    Add Sqlite provider.

---
+++ InvokeSqliteQuery.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Common;
+using System.Data.SQLite;
+using System.Management.Automation;
+
+namespace InvokeQuery
+{
+    [Cmdlet("Invoke", "SqliteQuery", SupportsTransactions = true, SupportsShouldProcess = true, ConfirmImpact = ConfirmImpact.Low)]
+    public  class InvokeSqliteQuery : InvokeQueryBase
+    {
+        protected override DbProviderFactory GetProviderFactory()
+        {
+            return SQLiteFactory.Instance;
+        }
+    }
+}
