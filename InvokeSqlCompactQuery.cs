080bf955de439c957953b01b360a6be0170f44c2
Steven Swenson <ctigeek@gmail.com>
2015-01-24 19:53:55 -0500

    Add Sql CE provider.

---
+++ InvokeSqlCompactQuery.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Common;
+using System.Data.SqlServerCe;
+using System.Management.Automation;
+
+namespace InvokeQuery
+{
+    [Cmdlet("Invoke", "SqlCompactQuery", SupportsTransactions = true, SupportsShouldProcess = true, ConfirmImpact = ConfirmImpact.Low)]
+    public  class InvokeSqlCompactQuery : InvokeQueryBase
+    {
+        protected override DbProviderFactory GetProviderFactory()
+        {
+            return SqlCeProviderFactory.Instance;
+        }
+    }
+}
