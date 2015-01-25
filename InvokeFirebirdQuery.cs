328aad6cc983196e00adf6a61cd7fdcc72b47293
Steven Swenson <ctigeek@gmail.com>
2015-01-24 19:53:08 -0500

    Add Firebird provider.

---
+++ InvokeFirebirdQuery.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Common;
+using System.Management.Automation;
+using FirebirdSql.Data.FirebirdClient;
+
+namespace InvokeQuery
+{
+    [Cmdlet("Invoke", "FirebirdQuery", SupportsTransactions = true, SupportsShouldProcess = true, ConfirmImpact = ConfirmImpact.Low)]
+    public  class InvokeFirebirdQuery : InvokeQueryBase
+    {
+        protected override DbProviderFactory GetProviderFactory()
+        {
+            return FirebirdClientFactory.Instance;
+        }
+    }
+}
