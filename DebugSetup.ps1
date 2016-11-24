bc95dc0895f3bbcc3885a6da1d4998aba4550c82
Steven Swenson <steven.swenson@rackspace.com>
2016-11-24 17:43:02 -0500

    This fixes issue #3, incorrect row count when piping in multiple text queries.

--- DebugSetup.ps1
+++ DebugSetup.ps1
@@ -5,7 +5,14 @@ Write-Host "Hello world"
 ## CREATE TABLE [dbo].[sometable]([id] [int] NOT NULL,	[somestring] [nvarchar](255) NOT NULL,	[someint] [int] NOT NULL,	[somedate] [datetime2](7) NOT NULL, CONSTRAINT [PK_sometable] PRIMARY KEY CLUSTERED ([id] ASC))
 ##
 
-$query = "select * from sometable;"
+$db = "test"
+$sql1 = "update table1 set someint = 4321 where pk = '1A9F96AE-54FA-40F9-9069-749F3ACF0CEF';"
+$sql2 = "update table1 set someint = 1234 where pk = '89F21B61-891B-42CD-9BED-84BA4B7EB7D0';"
+
+$count = ($sql1,$sql2) | Invoke-SqlServerQuery -Database $db -CUD -Verbose -ExpectedRowCount 3
+Write-Host "Count $count."
+
+#$query = "select * from sometable;"
 #$query | Invoke-SqlServerQuery -Database "test" -Verbose
 
 ##$params = @{"id"=7; "str"="hello"; "inte"=44; "dt"=(Get-Date); "blah"="blah"}
@@ -18,9 +25,9 @@ $query = "select * from sometable;"
 #} | Invoke-SqlServerQuery -Database "test" -CUD -Verbose
 
 
-171..175 | %{
-	New-SqlQuery -Sql "insert into sometable values (@id, @str, @inte, @dt);" -ExpectedRowCount 0  -Parameters @{ "id"=$_; "str"="hi"; "inte"=$_ + 99; "dt"=$(Get-Date); } -CUD
-	} | Invoke-SqlServerQuery -Database "test" -Verbose
+#171..175 | %{
+#	New-SqlQuery -Sql "insert into sometable values (@id, @str, @inte, @dt);" -ExpectedRowCount 0  -Parameters @{ "id"=$_; "str"="hi"; "inte"=$_ + 99; "dt"=$(Get-Date); } -CUD
+#	} | Invoke-SqlServerQuery -Database "test" -Verbose
 
 ##$query = "insert into sometable values (@id, @str, @inte, @dt);"
 ##$query | Invoke-SqlServerQuery -Database "test" -Parameters $params -NonQuery -Verbose
